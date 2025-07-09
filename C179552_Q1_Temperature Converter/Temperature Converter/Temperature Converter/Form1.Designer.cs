36d1e448895faeb3e8538a59d7b1dce85d0b657e
Saumya1982 <saumyapriyanganisenarathne@gmail.com>
2025-07-09 13:59:14 +0530

    Add files via upload

---
+++ C179552_Q1_Temperature Converter/Temperature Converter/Temperature Converter/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace Temperature_Converter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.lblInputTemp = new System.Windows.Forms.Label();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.rbCelsius = new System.Windows.Forms.RadioButton();
+            this.rbFahrenheit = new System.Windows.Forms.RadioButton();
+            this.lblSelect = new System.Windows.Forms.Label();
+            this.btnConvert = new System.Windows.Forms.Button();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.ForeColor = System.Drawing.Color.Teal;
+            this.lblTitle.Location = new System.Drawing.Point(107, 27);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(328, 32);
+            this.lblTitle.TabIndex = 0;
+            this.lblTitle.Text = "Temperature Converter";
+            // 
+            // lblInputTemp
+            // 
+            this.lblInputTemp.AutoSize = true;
+            this.lblInputTemp.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblInputTemp.Location = new System.Drawing.Point(18, 142);
+            this.lblInputTemp.Name = "lblInputTemp";
+            this.lblInputTemp.Size = new System.Drawing.Size(181, 25);
+            this.lblInputTemp.TabIndex = 1;
+            this.lblInputTemp.Text = "Enter Temperature:";
+            // 
+            // txtInput
+            // 
+            this.txtInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtInput.Location = new System.Drawing.Point(219, 139);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(123, 30);
+            this.txtInput.TabIndex = 2;
+            // 
+            // rbCelsius
+            // 
+            this.rbCelsius.AutoSize = true;
+            this.rbCelsius.Location = new System.Drawing.Point(202, 201);
+            this.rbCelsius.Name = "rbCelsius";
+            this.rbCelsius.Size = new System.Drawing.Size(161, 20);
+            this.rbCelsius.TabIndex = 3;
+            this.rbCelsius.TabStop = true;
+            this.rbCelsius.Text = "Fahrenheit To Celsius ";
+            this.rbCelsius.UseVisualStyleBackColor = true;
+            // 
+            // rbFahrenheit
+            // 
+            this.rbFahrenheit.AutoSize = true;
+            this.rbFahrenheit.Location = new System.Drawing.Point(204, 245);
+            this.rbFahrenheit.Name = "rbFahrenheit";
+            this.rbFahrenheit.Size = new System.Drawing.Size(161, 20);
+            this.rbFahrenheit.TabIndex = 4;
+            this.rbFahrenheit.TabStop = true;
+            this.rbFahrenheit.Text = "Celsius To Fahrenheit ";
+            this.rbFahrenheit.UseVisualStyleBackColor = true;
+            // 
+            // lblSelect
+            // 
+            this.lblSelect.AutoSize = true;
+            this.lblSelect.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSelect.Location = new System.Drawing.Point(108, 217);
+            this.lblSelect.Name = "lblSelect";
+            this.lblSelect.Size = new System.Drawing.Size(73, 25);
+            this.lblSelect.TabIndex = 5;
+            this.lblSelect.Text = "Select:";
+            // 
+            // btnConvert
+            // 
+            this.btnConvert.Location = new System.Drawing.Point(155, 296);
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.Size = new System.Drawing.Size(175, 33);
+            this.btnConvert.TabIndex = 6;
+            this.btnConvert.Text = "Convert";
+            this.btnConvert.UseVisualStyleBackColor = true;
+            this.btnConvert.Click += new System.EventHandler(this.btnConvert_Click);
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResult.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.lblResult.Location = new System.Drawing.Point(27, 383);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(94, 29);
+            this.lblResult.TabIndex = 7;
+            this.lblResult.Text = "Result:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(569, 450);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.btnConvert);
+            this.Controls.Add(this.lblSelect);
+            this.Controls.Add(this.rbFahrenheit);
+            this.Controls.Add(this.rbCelsius);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.lblInputTemp);
+            this.Controls.Add(this.lblTitle);
+            this.Name = "Form1";
+            this.Text = "Temperature Converter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Label lblInputTemp;
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.RadioButton rbCelsius;
+        private System.Windows.Forms.RadioButton rbFahrenheit;
+        private System.Windows.Forms.Label lblSelect;
+        private System.Windows.Forms.Button btnConvert;
+        private System.Windows.Forms.Label lblResult;
+    }
+}
+
---
+++ C179552_Q1_Temperature Converter/Temperature Converter/Temperature Converter/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Temperature_Converter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConvert_Click(object sender, EventArgs e)
+        {
+            // Try parsing user input
+            if (double.TryParse(txtInput.Text, out double temp))
+            {
+                double result;
+
+                if (rbCelsius.Checked)
+                {
+                    // Fahrenheit to Celsius
+                    result = (temp - 32) * 5 / 9;
+                    lblResult.Text = "Result: " + result.ToString("F2") + " °C";
+                }
+                else if (rbFahrenheit.Checked)
+                {
+                    // Celsius to Fahrenheit
+                    result = (temp * 9 / 5) + 32;
+                    lblResult.Text = "Result: " + result.ToString("F2") + " °F";
+                }
+                else
+                {
+                    lblResult.Text = "Please select a conversion unit.";
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please enter a valid numeric temperature.");
+            }
+        }
+    }
+}
